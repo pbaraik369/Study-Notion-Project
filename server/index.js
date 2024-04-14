5cd3cb156d79a62415d645b7e3b49f5eef45e585
pankaj baraik <101423423+pbaraik369@users.noreply.github.com>
2024-04-14 23:43:42 +0530

    second

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://study-notion-project-frontend-kappa.vercel.app",
+    origin: "*",
     credentials: true,
   })
 );
